e336789bc9f0f0e0de3d08efa66c50539c035058
Ihor Myroniuk <job.ihormyroniuk@gmail.com>
2019-07-29 14:48:06 +0300

    no message

---
+++ Example/DesignPatternMediatorExample/DesignPatternMediatorExample/Application.swift
@@ -0,0 +1,19 @@
+//
+//  Application.swift
+//  DesignPatternMediatorExample
+//
+//  Created by Ihor Myroniuk on 7/29/19.
+//  Copyright © 2019 Ihor Myroniuk. All rights reserved.
+//
+
+import UIKit
+import AUIKit
+
+class Application: AUIDefaultApplication {
+  
+  override func didFinishLaunching() {
+    super.didFinishLaunching()
+    
+  }
+  
+}
---
+++ Example/DesignPatternMediatorExample/DesignPatternMediatorExample/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  DesignPatternMediatorExample
+//
+//  Created by Ihor Myroniuk on 7/29/19.
+//  Copyright © 2019 Ihor Myroniuk. All rights reserved.
+//
+
+import UIKit
+
+UIApplicationMain(
+  CommandLine.argc, CommandLine.unsafeArgv,
+  NSStringFromClass(Application.self), NSStringFromClass(Application.self)
+)
+
+
